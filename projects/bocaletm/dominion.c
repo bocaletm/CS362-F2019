82fd19c3094d659da2a185ed10ea2d78b1205d66
Mario Bocaletti <bocaletm@oregonstate.edu>
2019-10-12 17:10:06 +0000

    refactor minion initial

--- projects/bocaletm/dominion.c
+++ projects/bocaletm/dominion.c
@@ -272,6 +272,62 @@ int mineEffect(int choice1, int choice2, int currentPlayer, int handPos, struct 
     return 0;
 }
 
+/*******************************
+ * MINION EFFECT
+ * ****************************/
+int minionEffect(int choice1, int currentPlayer, int handPos, struct gameState *state){
+    int i;
+    int j;
+    //+1 action
+    state->numActions++;
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+
+    if (choice1)
+    {
+        state->coins = state->coins + 2;
+    }
+    else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+    {
+        //discard hand
+        while(numHandCards(state) > 0)
+        {
+            discardCard(handPos, currentPlayer, state, 0);
+        }
+
+        //draw 4
+        for (i = 0; i < 4; i++)
+        {
+            drawCard(currentPlayer, state);
+        }
+
+        //other players discard hand and redraw if hand size > 4
+        for (i = 0; i < state->numPlayers; i++)
+        {
+            if (i != currentPlayer)
+            {
+                if ( state->handCount[i] > 4 )
+                {
+                    //discard hand
+                    while( state->handCount[i] > 0 )
+                    {
+                        discardCard(handPos, i, state, 0);
+                    }
+
+                    //draw 4
+                    for (j = 0; j < 4; j++)
+                    {
+                        drawCard(i, state);
+                    }
+                }
+            }
+        }
+
+    }
+
+    return 0;
+}
 
 /*******************************
  * BARON EFFECT
@@ -973,53 +1029,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return 0;
 
     case minion:
-        //+1 action
-        state->numActions++;
-
-        //discard card from hand
-        discardCard(handPos, currentPlayer, state, 0);
-
-		if (choice1)
-        {
-            state->coins = state->coins + 2;
-        }
-        else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-        {
-            //discard hand
-            while(numHandCards(state) > 0)
-            {
-                discardCard(handPos, currentPlayer, state, 0);
-            }
-
-            //draw 4
-            for (i = 0; i < 4; i++)
-            {
-                drawCard(currentPlayer, state);
-            }
-
-            //other players discard hand and redraw if hand size > 4
-            for (i = 0; i < state->numPlayers; i++)
-            {
-                if (i != currentPlayer)
-                {
-                    if ( state->handCount[i] > 4 )
-                    {
-                        //discard hand
-                        while( state->handCount[i] > 0 )
-                        {
-                            discardCard(handPos, i, state, 0);
-                        }
-
-                        //draw 4
-                        for (j = 0; j < 4; j++)
-                        {
-                            drawCard(i, state);
-                        }
-                    }
-                }
-            }
-
-        }
+        minionEffect(choice1, currentPlayer, handPos, state);
         return 0;
 
     case steward:
--- projects/bocaletm/dominion_helpers.h
+++ projects/bocaletm/dominion_helpers.h
@@ -10,7 +10,7 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
 int baronEffect(int choice1, int currentPlayer, int nextPlayer,  struct gameState *state);
-int minionEffect();
+int minionEffect(int choice1, int currentPlayer, int handPos, struct gameState *state);
 int ambassadorEffect();
 int tributeEffect();
 int mineEffect(int choice1, int choice2, int currentPlayer, int handPos, struct gameState *state);
