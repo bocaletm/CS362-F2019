a86977ec1f3c594ad6d526ac689476aac0d37e56
Mario Bocaletti <bocaletm@oregonstate.edu>
2019-11-29 21:06:31 +0000

    unit test for bug 5

---
+++ projects/FinalProject-Bugs/dominion/unittest5.c
@@ -0,0 +1,41 @@
+#include "unittests.h"
+
+/**************
+ * UNIT TEST 5: scoreFor function uses wrong count variable
+ *************/
+int unitTestFive() {
+  int failed = 0; //used to keep track of test status
+
+  // set your card array
+  int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
+
+  // declare the game state
+  struct gameState G;
+  struct gameState* gamePtr = &G;
+
+  //rand seed
+  time_t seed = time(0); 
+  printf("Begin Testing scoreFor() count variable:\n");
+
+  //new game
+  memset(gamePtr, 23, sizeof(struct gameState)); //set the game state
+  int r = initializeGame(2, k, (int)seed, gamePtr); //initialize a new game
+
+  //set the game state
+  gamePtr->handCount[0] = 0;
+  gamePtr->discardCount[0] = 0;
+  gamePtr->deckCount[0] = 1;
+  gamePtr->deck[0][0] = province; //should add 6 points
+  failed = assertMod(scoreFor(0,gamePtr),6);
+  if (failed) {
+    printf("failed to score deckCount\n");
+  }
+
+  printf("Test completed!\n");
+
+  return 0;
+}
+
+int main() {
+  unitTestFive();  
+}
